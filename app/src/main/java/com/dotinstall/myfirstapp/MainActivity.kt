370aa7be04e946474457f38a60852c0f3565a266
Tomonori Ueno <t.uepon@gmail.com>
2018-10-08 07:35:57 +0900

    Edited strings.xml

--- app/src/main/java/com/dotinstall/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/dotinstall/myfirstapp/MainActivity.kt
@@ -2,11 +2,17 @@ package com.dotinstall.myfirstapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun changeTextView(view: View){
+        messageTextView.text = "Hello World!"
     }
 }
